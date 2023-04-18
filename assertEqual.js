c3accbd7bae65994a921a14f60396e9c7adf2bd3
Katherine Struthers <ktstruthers@gmail.com>
2023-04-18 07:31:42 -0500

    replaced concats with template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("😁😁😁 Assertion Passed: " + actual + " === " + expected);
+    console.log(`😁😁😁 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("😡😡😡 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
