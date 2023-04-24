6af4b237efaf276bfc51fcd4adf2f7bfa5ba31e0
Katherine Struthers <ktstruthers@gmail.com>
2023-04-24 13:02:50 -0500

    function to return an object listing letter positions in a string

---
+++ letterPositions.js
@@ -0,0 +1,42 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`😁😁😁 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const letterPositions = function(string) {
+  const results = {};
+
+  for (let i = 0; i < string.length; i++) {
+    if (results[string[i]]) {
+      results[string[i]].push(i);
+    } else {
+      results[string[i]] = [i];
+    }
+  }
+
+  return results;
+};
+
+// testing
+const result1 = letterPositions("hello");
+assertArraysEqual(result1["h"], [0]);
+assertArraysEqual(result1["e"], [1]);
+assertArraysEqual(result1["l"], [2, 3]);
+assertArraysEqual(result1["o"], [4]);
