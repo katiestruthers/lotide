138ebbbd90115fc3b9d6255d4d28d2898cd63d31
Katherine Struthers <ktstruthers@gmail.com>
2023-04-24 14:34:43 -0500

    function to find a property within an object that has a given value

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😁😁😁 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  for (const property in object) {
+    if (object[property] === value) {
+      return property;
+    }
+  }
+};
+
+// testing
+const bestTVShowsByGenre = {
+  "sci_fi": "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "drama"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, ""), undefined);
