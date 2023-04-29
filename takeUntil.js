8bf8d9a2732c0bde8df0679b7850bb69b26a95d0
Katherine Struthers <ktstruthers@gmail.com>
2023-04-29 13:39:04 -0500

    function to mimic Lodash takeWhile

---
+++ takeUntil.js
@@ -0,0 +1,41 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`😁😁😁 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const takeUntil = function(array, callback) {
+  const returnArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) {
+      break;
+    }
+    returnArray.push(array[i]);
+  }
+
+  return returnArray;
+};
+
+// testing
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+assertArraysEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2]);
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+assertArraysEqual(takeUntil(data2, x => x === ','), ['I\'ve', 'been', 'to', 'Hollywood']);
