93142d13a67f94c434cf8b16d4205186c8297da3
Katherine Struthers <ktstruthers@gmail.com>
2023-05-10 10:18:07 -0500

    move tests into seperate file

--- head.js
+++ head.js
@@ -1,11 +1,5 @@
-const assertEqual = require('./assertEqual');
-
 const head = function(array) {
   return array[0];
 };
 
-// testing
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5]), 5);
-assertEqual(head([]), undefined);
+module.exports = head;
